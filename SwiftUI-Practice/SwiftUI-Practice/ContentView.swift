f736f24149d9cf05262c44c4639f3d976c31074c
Sonjh1306 <yale1306@gmail.com>
2024-03-11 14:35:10 +0900

    feat: 프로젝트 생성

---
+++ SwiftUI-Practice/SwiftUI-Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Practice
+//
+//  Created by sonjuhyeong on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Practice/SwiftUI-Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI-Practice
+//
+//  Created by sonjuhyeong on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
