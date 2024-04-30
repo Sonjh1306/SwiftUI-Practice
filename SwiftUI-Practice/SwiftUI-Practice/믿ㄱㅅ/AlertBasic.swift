e679a7d3739368fc5710ced408dd86f7390d8de5
Sonjh1306 <yale1306@gmail.com>
2024-04-30 14:00:11 +0900

    Alert

---
+++ SwiftUI-Practice/SwiftUI-Practice/믿ㄱㅅ/AlertBasic.swift
@@ -0,0 +1,18 @@
+//
+//  AlertBasic.swift
+//  SwiftUI-Practice
+//
+//  Created by sonjuhyeong on 4/30/24.
+//
+
+import SwiftUI
+
+struct AlertBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AlertBasic()
+}
