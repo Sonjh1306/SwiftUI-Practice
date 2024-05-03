4f851e9868ca63463e5f5b8d727d5075efe90557
Sonjh1306 <yale1306@gmail.com>
2024-05-03 15:31:45 +0900

    ColorPicker

---
+++ SwiftUI-Practice/SwiftUI-Practice/ColorPicker/ColorPickerBasic.swift
@@ -0,0 +1,34 @@
+//
+//  ColorPickerBasic.swift
+//  SwiftUI-Practice
+//
+//  Created by sonjuhyeong on 5/3/24.
+//
+
+import SwiftUI
+
+struct ColorPickerBasic: View {
+    
+    // Property
+    @State var backgroundColor: Color = .green
+    
+    var body: some View {
+        ZStack {
+            backgroundColor.ignoresSafeArea()
+            
+            ColorPicker(selection: $backgroundColor, supportsOpacity: true, label: {
+                Text("원하는 색을 고르세요.")
+            })
+            .padding()
+            .background(.black)
+            .cornerRadius(10)
+            .foregroundColor(.white)
+            .font(.headline)
+            .padding(50)                
+        }
+    }
+}
+
+#Preview {
+    ColorPickerBasic()
+}
